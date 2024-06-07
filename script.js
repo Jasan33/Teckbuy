7e0ec5c44aec9f6cc8529bc8bb3c489b57a84e12
Jasan33 <Jasandeepskole@gmail.com>
2024-06-07 13:19:24 +0200

    Version 1
    This is the first version not the last

---
+++ script.js
@@ -0,0 +1,114 @@
+function Meny() {
+    let y = document.getElementById("meny");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+}
+
+function loginn() {
+    let x = document.getElementById("loginn");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function signup() {
+  let signupInfo = document.getElementById("signup");
+  let signupText = document.getElementById("signup_here");
+  let logInfo = document.getElementById("log_information");
+
+  if (signupInfo.style.display === "none" ) {
+      signupInfo.style.display = "block";
+      signupText.textContent = "Log in";
+      logInfo.style.display = "none";
+  } else {
+      signupInfo.style.display = "none";
+      signupText.textContent = "Signup here";
+      logInfo.style.display = "block";
+  }
+}
+
+function forgot() {
+  let x = document.getElementById("forgot");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function Iphone() {
+    let x = document.getElementById("Iphone");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function X_Iphone() {
+    let x = document.getElementById("Iphone");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function Gaming_pc() {
+    let x = document.getElementById("Gaming_pc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function X_gaming_pc() {
+    let x = document.getElementById("Gaming_pc");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function TV() {
+    let x = document.getElementById("TV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function X_TV() {
+    let x = document.getElementById("TV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function Apple_vision() {
+    let x = document.getElementById("Apple_vision");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function X_Apple_vision() {
+    let x = document.getElementById("Apple_vision");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
